677980b4f92bf106c90139cb5d332509b3d5e8c0
AFLLAMA <AFLLAMA@users.noreply.github.com>
2018-09-27 15:41:38 +0100

    question1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+int main()
+{
+	int a = 1;
+	int* b = &a;
+	long c = 10101010l;
+	double d1 = 1.1;
+	double* d2 = &d1;
+	char ch1 = 'a';
+	char* ch2 = &ch1;
+	char** ch3 = &ch2;
+
+	printf("%d\n%d\n%d\n%d\n%d", sizeof(a), sizeof(b), sizeof(c), sizeof(d2), sizeof(ch3));
 }
